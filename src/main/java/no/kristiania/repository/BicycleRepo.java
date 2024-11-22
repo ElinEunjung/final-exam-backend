692ccc0650c5d6736bea058af54ae576b83495f9
ElinEunjung <eunjungp615@gmail.com>
2024-11-22 15:48:30 +0100

    Delete unnecessary files

--- src/main/java/no/kristiania/repository/BicycleRepo.java
+++
@@ -1,7 +0,0 @@
-package no.kristiania.repository;
-
-import no.kristiania.domain.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-//JpaRepository offers basic CRUD methods, paging & array
-public interface BicycleRepo extends JpaRepository<Customer, Long> {
-}
--- src/main/java/no/kristiania/repository/ManufacturerRepo.java
+++
@@ -1,7 +0,0 @@
-package no.kristiania.repository;
-
-import no.kristiania.domain.CustomerAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManufacturerRepo extends JpaRepository<CustomerAddress,Long> {
-}
